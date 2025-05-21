4ff296ed36377ead6591d019c3cdc71ba688c926
IwataRisa1996 <risaiwata1996@gmail.com>
2025-05-21 12:28:35 +0900

    背景画像の変更、cssの追加

--- portfolio-app/src/components/Hero.jsx
+++ portfolio-app/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "../styles/layout.css";
 
 const Hero = () => {
   return (
-    <section className="bluegray2 text-white text-center py-5">
+    <section className="BGimg text-white text-center py-5">
       <div className="container-fluid">
         <h2 className="display-4">Risa Iwata</h2>
         <p className="lead">Developer</p>
